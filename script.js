3a94b9784b7fa4b7c5749a1de1db5e61a06c61fb
fatbardheminii <fatbardh.emini@hotmail.com>
2023-12-23 21:20:36 +0100

    create 3 func to validate buttons
    rainbow, delete, pickColor btn validated through  new func and listeners

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const gridWrapper = document.querySelector('#grid-wrapper');
 const gridSizeRange = document.querySelector('#grid-size-range');
 const gridDivColor = document.querySelector('#grid-div-color');
 const gridDivBgColor = document.querySelector("#grid-div-bg-color");
-const randomColor = document.querySelector("#random-color");
+const rainbowColor = document.querySelector("#rainbow-color");
 const deleteColor = document.querySelector("#delete-color");
 const addBorders = document.querySelector("#add-borders");
 const deleteBorders = document.querySelector("#delete-borders");
@@ -15,7 +15,8 @@ function fillGridArea(size){
      for (let i = 0; i < size.value * size.value; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.toggle('grid-item');
-        gridItem.style.cssText = `border:1px solid black;display:inline-block`;
+        gridItem.style.cssText = `border:1px solid black;display:inline-block;background-color:${gridDivBgColor.value}`;
+        gridItem.addEventListener("mouseenter", () => changeColor(gridItem));
         gridWrapper.appendChild(gridItem);
     }
 }
@@ -26,18 +27,60 @@ function setGridLayout(num){
 }
 
 //listener when page is loaded gridWrapper size = rangeSize.value
-window.addEventListener("load", function(){
+window.addEventListener("load", () => {
     fillGridArea(gridSizeRange)
     setGridLayout(gridSizeRange);
 });
-
-gridSizeRange.addEventListener("change", function () {
+//listener when size of grid is changed
+gridSizeRange.addEventListener("change", () => {
   //innerHTML deletes all old child elements after every change !!
   gridWrapper.innerHTML = "";
   rangeSize.textContent = `${gridSizeRange.value}`;
+  rainbowColor.classList.remove('active-button');
+  deleteColor.classList.remove("active-button");
+  currentMode = 'color';
   fillGridArea(gridSizeRange);
   setGridLayout(gridSizeRange);
 });
 
+let currentMode = 'color';
+//select color to be used in changeColor func
+function setCurrentMode(newMode){
+    activeButton(newMode)
+    currentMode = newMode;
+}
+//css applied to active button
+function activeButton(newMode){
+    if(currentMode === 'rainbow'){
+        rainbowColor.classList.remove('active-button');
+    } else if(currentMode === 'delete'){
+        deleteColor.classList.remove('active-button');
+    }
+
+    if(newMode === 'rainbow'){
+        rainbowColor.classList.add('active-button');
+    } else if(newMode === 'delete') {
+        deleteColor.classList.add('active-button');
+    }
+}
+
+//change gridItem color
+function changeColor(gridItem){
+    if(currentMode === 'color'){
+        gridItem.style.backgroundColor = `${gridDivColor.value}`;
+    } else if(currentMode === 'rainbow'){
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        gridItem.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } else if(currentMode === 'delete'){
+        gridItem.style.backgroundColor = `${gridDivBgColor.value}`;
+    }
+    
+}
+
+rainbowColor.addEventListener("click",() => setCurrentMode('rainbow'));
+gridDivColor.addEventListener("click", () => setCurrentMode('color'));
+deleteColor.addEventListener("click", () => setCurrentMode('delete'));
 
 
